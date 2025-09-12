b9c874582b684deca8862a1b0cfb7145faf7cfa6
thetrueryan <georgytoper@gmail.com>
2025-09-12 09:59:43 +0300

    add sql table model

--- src/models/sqlmodels.py
+++ src/models/sqlmodels.py
@@ -1,11 +1,23 @@
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+from typing import Annotated
+
+intpk = Annotated[int, mapped_column(primary_key=True)]
+
+created_at = Annotated[
+    datetime, mapped_column(server_default=text("TIMEZONE ('utc', now())"))
+]
 
 
-class Base(DeclarativeBase):
-    ...
+class Base(DeclarativeBase): ...
 
 
 class ScheduleOrm(Base):
     __tablename__ = "schedule"
 
-    
+    id: Mapped[intpk]
+    day: Mapped[str] = mapped_column(nullable=True)
+    hours: Mapped[str] = mapped_column(nullable=True)
+    lesson: Mapped[str] = mapped_column(nullable=True)
+    teacher: Mapped[str] = mapped_column(nullable=True)
+    created_at: Mapped[created_at]
